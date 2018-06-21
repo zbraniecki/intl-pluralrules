e9b09522f8708fd6a64eb95efa7db04f87931562
unclenachoduh <kekoariggin@gmail.com>
2018-06-20 17:31:17 -0700

    Refactor of plural operand getter and test

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-extern crate intl_pluralrules;
-
-// use intl_pluralrules::*;
-
-fn main() {
-	println!("{:?}", intl_pluralrules::PluralOperands::new("5"));
-	println!("{:?}", intl_pluralrules::PluralOperands::new("55.0"));
-}
